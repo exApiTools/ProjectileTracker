d0874fe26cc8ab69ea9862b5d94620f67527a299
instant.sc <code@insc.cc>
2023-10-05 01:17:45 +0200

    Add project files.

---
+++ ProjectileTracker.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using ExileCore;
+using ExileCore.PoEMemory.Components;
+using ExileCore.PoEMemory.MemoryObjects;
+using ExileCore.Shared.Helpers;
+using SharpDX;
+using Vector2 = System.Numerics.Vector2;
+using Vector4 = System.Numerics.Vector4;
+
+namespace ProjectileTracker;
+
+public class ProjectileTracker : BaseSettingsPlugin<ProjectileTrackerSettings>
+{
+    private readonly HashSet<Entity> _trackedEntities = new HashSet<Entity>();
+    private readonly Stopwatch _refreshSw = Stopwatch.StartNew();
+
+    public override bool Initialise()
+    {
+        return true;
+    }
+
+    public override void AreaChange(AreaInstance area)
+    {
+        _trackedEntities.Clear();
+    }
+
+    public override Job Tick()
+    {
+        if (_refreshSw.Elapsed > TimeSpan.FromMilliseconds(Settings.FullRefreshPeriod))
+        {
+            _refreshSw.Restart();
+            _trackedEntities.Clear();
+            var entitiesToAdd = GameController.EntityListWrapper.Entities.Where(IsMatchingEntity).ToList();
+            foreach (var entity in entitiesToAdd)
+            {
+                _trackedEntities.Add(entity);
+            }
+        }
+
+        return null;
+    }
+
+    public override void Render()
+    {
+        var playerPos = GameController.Player.GridPosNum;
+        foreach (var trackedEntity in _trackedEntities.Where(x => x.IsValid))
+        {
+            var entityPos = trackedEntity.GridPosNum;
+            var targetPos = trackedEntity.GetComponent<Positioned>().TravelTarget.WorldToGrid();
+            var xy = entityPos - targetPos;
+            var xz = entityPos - playerPos;
+            var xylsq = xy.LengthSquared();
+            float minDistance;
+            if (xylsq == 0)
+            {
+                minDistance = 0;
+            }
+            else
+            {
+                var t = Math.Clamp(Vector2.Dot(xy, xz) / xylsq, 0, 1);
+                minDistance = (Vector2.Lerp(entityPos, targetPos, t) - playerPos).Length();
+            }
+
+            var color = minDistance <= Settings.MinDistance
+                ? Settings.MaxDangerLineColor.Value.ToImguiVec4()
+                : Vector4.Lerp(Settings.MediumDangerLineColor.Value.ToImguiVec4(), Settings.LeastDangerLineColor.Value.ToImguiVec4(),
+                    Math.Clamp((minDistance - Settings.MinDistance) / (Settings.MaxDistance - Settings.MinDistance), 0, 1));
+            var entityScreenPos = GameController.IngameState.Data.GetGridScreenPosition(entityPos);
+            var targetScreenPos = GameController.IngameState.Data.GetGridScreenPosition(targetPos);
+            Graphics.DrawLine(entityScreenPos, targetScreenPos, 10, color.ToSharpColor());
+        }
+    }
+
+    public override void EntityAdded(Entity entity)
+    {
+        if (IsMatchingEntity(entity))
+        {
+            _trackedEntities.Add(entity);
+        }
+    }
+
+    private bool IsMatchingEntity(Entity entity)
+    {
+        return entity.HasComponent<Positioned>() && Settings.EntityPatterns.Select(x => new Regex(x)).Any(x => x.IsMatch(entity.Path));
+    }
+}
---
+++ ProjectileTrackerSettings.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+using ImGuiNET;
+using Newtonsoft.Json;
+using SharpDX;
+
+namespace ProjectileTracker;
+
+public class ProjectileTrackerSettings : ISettings
+{
+    public ProjectileTrackerSettings()
+    {
+        PatternEditor = new CustomNode(() =>
+        {
+            for (int i = 0; i < EntityPatterns.Count; i++)
+            {
+                var str = EntityPatterns[i];
+                if (ImGui.InputText($"##{i}", ref str, 2000))
+                {
+                    EntityPatterns[i] = str;
+                }
+
+                ImGui.SameLine();
+                if (ImGui.Button("X"))
+                {
+                    EntityPatterns.RemoveAt(i);
+                }
+            }
+
+            if (ImGui.Button("+"))
+            {
+                EntityPatterns.Add("^$");
+            }
+        });
+    }
+
+    public ToggleNode Enable { get; set; } = new ToggleNode(false);
+    public ColorNode MaxDangerLineColor { get; set; } = Color.Red;
+    public ColorNode MediumDangerLineColor { get; set; } = Color.DarkGoldenrod;
+    public ColorNode LeastDangerLineColor { get; set; } = Color.DarkGoldenrod with { A = 128 };
+
+    public RangeNode<float> MinDistance { get; set; } = new RangeNode<float>(10, 0, 100);
+    public RangeNode<float> MaxDistance { get; set; } = new RangeNode<float>(40, 0, 100);
+    public RangeNode<int> FullRefreshPeriod { get; set; } = new RangeNode<int>(250, 1, 1000);
+
+    [JsonIgnore]
+    public CustomNode PatternEditor { get; set; }
+
+    public List<string> EntityPatterns = new List<string>();
+}
